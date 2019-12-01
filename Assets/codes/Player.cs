fa04a27d5f181f9c8cf720935417dc3db2d95472
gianleck <gianleck0@gmail.com>
2019-12-01 16:26:00 +0300

    Karakter Hareketi
    Karakter Hareketi prototipi, sahne protoripi, deneme amaçlı

--- Assets/codes/Player.cs
+++ Assets/codes/Player.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody2D physic;
+    public float speed = 3;
+    public float jumpspeed = 300;
+    float horizontal = 0f;
+    Vector3 vec;
+    bool onetimejump = true;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (onetimejump)
+            {
+                physic.AddForce(new Vector2(0, jumpspeed));
+                onetimejump = false;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        vec = new Vector3(horizontal * speed, physic.velocity.y, 0);
+
+        physic.velocity = vec;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        onetimejump = true;
     }
 }
